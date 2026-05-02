c97fa53bde3b8a46ad2a2d6f16fe09ba6b985c71
sugarraysam <samuel.blaisdowdy@protonmail.com>
2026-05-02 09:46:17 -0700

    Adds eBPF build script Configures the build.rs script to compile the eBPF C program and generate its Rust skeleton.

---
+++ build.rs
@@ -0,0 +1,29 @@
+//! Build script for the `profiler` eBPF program.
+
+use std::env;
+use std::ffi::OsStr;
+use std::path::PathBuf;
+
+use libbpf_cargo::SkeletonBuilder;
+
+const SRC: &str = "src/bpf/profiler.bpf.c";
+const HEADER: &str = "src/bpf/profiler.h";
+
+fn main() {
+    let out = PathBuf::from(env::var_os("OUT_DIR").expect("OUT_DIR must be set in build script"))
+        .join("profiler.skel.rs");
+
+    let arch = env::var("CARGO_CFG_TARGET_ARCH")
+        .expect("CARGO_CFG_TARGET_ARCH must be set in build script");
+
+    SkeletonBuilder::new()
+        .source(SRC)
+        .clang_args([
+            OsStr::new("-I"),
+            vmlinux::include_path_root().join(arch).as_os_str(),
+        ])
+        .build_and_generate(&out)
+        .unwrap();
+    println!("cargo:rerun-if-changed={SRC}");
+    println!("cargo:rerun-if-changed={HEADER}");
+}
